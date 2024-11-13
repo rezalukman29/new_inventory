5747e8ee11afa3bd0b8e92b20b8f16eb32ea4cc0
rezalukman29 <lukman.reza@gmail.com>
2024-11-13 20:14:37 +0700

    fix metadata

--- app/(full-page)/layout.tsx
+++ app/(full-page)/layout.tsx
@@ -7,8 +7,8 @@ interface SimpleLayoutProps {
 }
 
 export const metadata: Metadata = {
-    title: 'PrimeReact Sakai',
-    description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.'
+    title: 'EMI Inventory',
+    description: 'Inventory Management System'
 };
 
 export default function SimpleLayout({ children }: SimpleLayoutProps) {
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,16 +6,16 @@ interface AppLayoutProps {
 }
 
 export const metadata: Metadata = {
-    title: 'PrimeReact Sakai',
-    description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
+    title: 'EMI Inventory',
+    description: 'Inventory Management System',
     robots: { index: false, follow: false },
     viewport: { initialScale: 1, width: 'device-width' },
     openGraph: {
         type: 'website',
-        title: 'PrimeReact SAKAI-REACT',
-        url: 'https://sakai.primereact.org/',
-        description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
-        images: ['https://www.primefaces.org/static/social/sakai-react.png'],
+        title: 'EMI Inventory',
+        url: '',
+        description: 'Inventory Management System',
+        images: '',
         ttl: 604800
     },
     icons: {
