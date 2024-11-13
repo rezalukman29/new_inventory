dfcc2e72e9f0de38a86f06f41672aa012b49f036
rezalukman29 <lukman.reza@gmail.com>
2024-11-13 19:57:40 +0700

    initiate inventory

--- layout/AppFooter.tsx
+++ layout/AppFooter.tsx
@@ -8,9 +8,8 @@ const AppFooter = () => {
 
     return (
         <div className="layout-footer">
-            <img src={`/layout/images/logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`} alt="Logo" height="20" className="mr-2" />
             by
-            <span className="font-medium ml-2">PrimeReact</span>
+            <span className="font-medium ml-2">EMI 2024</span>
         </div>
     );
 };
