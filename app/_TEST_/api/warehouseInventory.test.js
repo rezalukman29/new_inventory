71618249c5e522de6af9c0e7a1a2c99dec91dcb4
rezalukman29 <lukman.reza@gmail.com>
2025-04-15 05:42:23 +0700

    add testing inventory and warehouse inventory

---
+++ app/_TEST_/api/warehouseInventory.test.js
@@ -0,0 +1,21 @@
+
+const fetch = require("isomorphic-fetch");
+
+async function getInventory() {
+  const response = await fetch(
+    "https://dev.emi-project.my.id/v1/barang-gudang/detail?page=1&limit=10",
+    {
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        "User-Id": 8
+      },
+    }
+  );
+  const data = await response.json();
+  return data;
+}
+
+test("Warehouse Inventory testing", async () => {
+  const data = await getInventory();
+  expect(data.success).toEqual(true);
+});
